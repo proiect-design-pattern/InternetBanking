ebabc33d0098c0be1c71fbd4c60a04c8e2cb254b
GabrielaAnghel <gabriela.loredana.anghel@gmail.com>
2019-04-13 19:30:10 +0300

    Added cash register interface.

---
+++ InternetBanking/StructuralPatterns/Flyweight/ICashRegister.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+using InternetBanking.StructuralPatterns.Flyweight.Currencies;
+
+namespace InternetBanking.StructuralPatterns.Flyweight
+{
+   public interface ICashRegister
+   {
+       void CacheOut(double val);
+   }
+}
