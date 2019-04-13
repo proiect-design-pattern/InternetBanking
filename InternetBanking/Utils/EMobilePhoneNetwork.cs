9ee4cb1dedb6dd8734e00e9e3bcbb29df9292e27
GabrielaAnghel <gabriela.loredana.anghel@gmail.com>
2019-04-13 13:33:18 +0300

    Added some utils classes.

---
+++ InternetBanking/Utils/EMobilePhoneNetwork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetBanking.Utils
+{
+   public enum EMobilePhoneNetwork
+   {
+      Telekom,
+      Vodafone,
+      Digi,
+      Orange,
+   }
+}
