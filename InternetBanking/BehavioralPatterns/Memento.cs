75b9f1138653b95a42e7f5223c3aed42a780dc8b
GabrielaAnghel <gabriela.loredana.anghel@gmail.com>
2019-04-13 15:26:58 +0300

    Changed visibility of classes.

--- InternetBanking/BehavioralPatterns/Memento.cs
+++ InternetBanking/BehavioralPatterns/Memento.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace InternetBanking.BehavioralPatterns
 {
-   class Memento
+   public class Memento
    {
    }
 }
--- InternetBanking/CreationalPatterns/Singleton.cs
+++ InternetBanking/CreationalPatterns/Singleton.cs
@@ -3,10 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using InternetBanking.StructuralPatterns.Interfaces;
 
 namespace InternetBanking.CreationalPatterns
 {
-   class Singleton
+   public class ClientAccount : IAccount
    {
+      private readonly long _id;
+      private string _password;
+      private static ClientAccount _instance;
+
+      public double Balance { get; set;}
+
+      public long ID
+      {
+         get { return _id; }
+      }
+      public ClientAccount()
+      {
+         _id = 10000;
+         _password = "1q2w3e";
+      }
+      public static ClientAccount Instance()
+      {
+            object locker = new object();
+            if (_instance == null) {
+               lock (locker) {
+                  if (_instance == null) {
+                     _instance = new ClientAccount();
+                  }
+               }
+            }
+            return _instance;
+      }
+      public void DisplayBalance()
+      {
+         Console.WriteLine(Balance);
+      }
+
+
+      public bool Activate(string oldPassword, string newPassword)
+      {
+         if (Login(oldPassword))
+         {
+            _password = newPassword;
+            return true;
+         }
+
+         return false;
+      }
+
+      public bool Login(string password)
+      {
+         if (_password.Equals(password))
+         {
+            return true;
+         }
+
+         return false;
+      }
    }
 }
