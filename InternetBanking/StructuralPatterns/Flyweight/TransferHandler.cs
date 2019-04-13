639a4e97c6fd355ad0be447da9ab6704c09943f4
BarthaRobert <roby_robert05@yahoo.com>
2019-04-13 16:11:46 +0300

    Adding Flyweight classes

--- InternetBanking/StructuralPatterns/Flyweight/TransferHandler.cs
+++ InternetBanking/StructuralPatterns/Flyweight/TransferHandler.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InternetBanking.StructuralPatterns
+namespace InternetBanking.StructuralPatterns.Flyweight
 {
-   public class Flyweight
-   {
-   }
+    class TransferHandler
+    {
+    }
 }
