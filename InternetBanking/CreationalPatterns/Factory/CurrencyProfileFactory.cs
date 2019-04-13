3909f366693a3c2459156b123c052b401c1c00d0
BarthaRobert <roby_robert05@yahoo.com>
2019-04-13 19:14:04 +0300

    Adding Factory code

---
+++ InternetBanking/CreationalPatterns/Factory/CurrencyProfileFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetBanking.CreationalPatterns.Factory
+{
+    public class CurrencyProfileFactory
+    {
+        public ICurrencyProfile CurrencyType(ECurrencyProfile currencyProfile)
+        {
+            ICurrencyProfile currency = null;
+            switch(currencyProfile)
+            {
+                case ECurrencyProfile.EUR:
+                    {
+                        currency = new ECurrencyProfile.EUR();
+                        break;
+                    }
+            }
+            return currency;
+        }
+    }
+}
