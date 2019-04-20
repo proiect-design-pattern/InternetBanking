a40a91467eb28701658bdd303d8345bc7d5d302d
BarthaRobert <roby_robert05@yahoo.com>
2019-04-20 21:28:31 +0300

    IAccount update

--- InternetBanking/StructuralPatterns/Interfaces/IAccount.cs
+++ InternetBanking/StructuralPatterns/Interfaces/IAccount.cs
@@ -8,8 +8,20 @@ namespace InternetBanking.StructuralPatterns.Interfaces
 {
    public interface IAccount
    {
-      void DisplayBalance();
-      void Deposit(double value);
+      //void DisplayBalance();
+      //void Deposit(double value);
+      void DisplayEurBalance();
+      void DisplayRonBalance();
+      void DisplayUsdBalance();
+
+      void DepositEur(double value);
+      void DepositRon(double value);
+      void DepositUsd(double value);
+
+      bool TransferEur(double value);
+      bool TransferRon(double value);
+      bool TransferUsd(double value);
+
       bool Activate(string oldPassword);
       bool Login(string password);
    }
